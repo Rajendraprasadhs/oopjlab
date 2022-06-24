b2e6f9f8139639fd4b59f8aa7c2a1431e6d13e9b
Rajendraprasadhs <108111912+Rajendraprasadhs@users.noreply.github.com>
2022-06-24 09:46:03 +0530

    Add files via upload

---
+++ program1/prg1_a.java
@@ -0,0 +1,32 @@
+package program1;
+
+import java.util.Scanner;
+//import java.util.*;
+
+public class prg1_a {
+   
+    public static void main(String[] args){
+       
+        Scanner sc= new Scanner(System.in);
+   
+         System.out.println("Enter the number you want to get factorial");
+         int n=sc.nextInt();
+         
+         int fact=1;
+         for(int i=1;i<=n;i++)
+         {
+              fact=fact*i;
+         }
+         
+         System.out.println("The factorial of "+n+" is "+fact);
+         
+    }
+}   
+   
+
+
+    
+
+
+    
+
